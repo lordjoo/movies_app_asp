2118176421d57764589c6498c754bf6474915fcd
Youssef <youssef.afify122@gmail.com>
2021-05-27 11:08:20 +0200

    Hi

---
+++ MovieU/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MovieU.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
