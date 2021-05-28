ddded46c47b3b9bd9eaa2e757a2bc36af977e40c
AhmedGamal77823 <ahmedgamale.1019@gmail.com>
2021-05-29 00:49:04 +0200

    final commit

---
+++ MovieU/Data/Migrations/20210528205041_Movies.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieU.Data.Migrations
+{
+    public partial class Movies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    Photo = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    StreamUrl = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] {"Name", "Photo", "StreamUrl"},
+                values: new object[] {"Shreck!", "p/shreck.jpg", "Https: //ok.ru"}
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
