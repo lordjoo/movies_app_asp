8009dbdce9cdb763aad47c4b2611eb92ad6ddb3e
Youssef <youssef.afify122@gmail.com>
2021-05-29 00:32:23 +0200

    add views and add TvShow Model

--- MovieU/Data/ApplicationDbContext.cs
+++ MovieU/Data/ApplicationDbContext.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MovieU.Models;
 
 namespace MovieU.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<TvShow> TvShows { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
