6c299b51f7dda18228be9313c107f1573aa7ac32
Youssef <youssef.afify122@gmail.com>
2021-05-30 01:07:32 +0200

    add tvshow migration

---
+++ MovieU/Data/Migrations/20210529230450_TvShows.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieU.Data.Migrations
+{
+    public partial class TvShows : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TvShow",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Episode = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    Photo = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    StreamUrl = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TvShow", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+            migrationBuilder.DropTable(
+                name: "TvShow");
+        }
+    }
+}
