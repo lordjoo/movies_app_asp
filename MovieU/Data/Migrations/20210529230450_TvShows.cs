e9fc7d47bd4b9910dcc440c2d95d359a378070bd
Youssef <youssef.afify122@gmail.com>
2021-05-30 04:54:06 +0200

    i'm sleepy

--- MovieU/Data/Migrations/20210529230450_TvShows.cs
+++ MovieU/Data/Migrations/20210529230450_TvShows.cs
@@ -8,7 +8,7 @@ namespace MovieU.Data.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "TvShow",
+                name: "TvShows",
                 columns: table => new
                 {
                     Id = table.Column<int>(type: "INTEGER", nullable: false)
