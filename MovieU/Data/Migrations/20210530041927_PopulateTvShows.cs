8d4e2cb45d49251fd417def3503fbb25a23d9926
Youssef <youssef.afify122@gmail.com>
2021-05-30 06:21:23 +0200

    add populate tv shows

---
+++ MovieU/Data/Migrations/20210530041927_PopulateTvShows.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieU.Data.Migrations
+{
+    public partial class PopulateTvShows : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "TvShows",
+                columns: new[] {"Name", "Photo", "Episode", "StreamUrl"},
+                values: new object[,]
+                {
+                    {"The Flash", "https://i.ibb.co/Y0xjvPW/5f6c62cfc5167.jpg", 1, "https://vidhd.org/embed-gc1kthk5nsmt.html"}, 
+                    {"The Flash", "https://i.ibb.co/Y0xjvPW/5f6c62cfc5167.jpg", 2, "https://vidhd.org/embed-6jm6ixttkbtc.html"},
+                    {"The Flash", "https://i.ibb.co/Y0xjvPW/5f6c62cfc5167.jpg", 3, "https://vidhd.org/embed-45156enomefa.html"},
+                    
+                    {"Agents of S.H.I.E.L.D", "https://i.ibb.co/zRhDW16/5f42f822dff4a.jpg", 1, "https://vidhd.org/embed-2pakoghiur6d.html"},
+                    {"Agents of S.H.I.E.L.D", "https://i.ibb.co/zRhDW16/5f42f822dff4a.jpg", 1, "https://vidhd.org/embed-efkgeif76hw2.html"},
+                    
+                    {"The BlackList", "https://i.ibb.co/j56RGfY/tt2741602.jpg", 12, "https://vidhd.org/embed-ixnzo1zshf8k.html"},
+                    
+                    
+                }
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
