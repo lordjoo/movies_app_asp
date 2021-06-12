33860e9b8b53ff53383e27e3a060346cf3cd5be9
Abdullah <abdullah.ayoub2000@gmail.com>
2021-06-12 18:46:33 +0200

    Add movies and tv shows :>

--- MovieU/Controllers/AdminMoviesController.cs
+++ MovieU/Controllers/AdminMoviesController.cs
@@ -56,7 +56,7 @@ namespace MovieU.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Photo,StreamUrl")] Movies movies)
+        public async Task<IActionResult> Create([Bind("Id,Name,Photo,StreamUrl, Tags")] Movies movies)
         {
             if (ModelState.IsValid)
             {
@@ -88,7 +88,7 @@ namespace MovieU.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Photo,StreamUrl")] Movies movies)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Photo,StreamUrl, Tags")] Movies movies)
         {
             if (id != movies.Id)
             {
--- MovieU/Controllers/AdminTvShowsController.cs
+++ MovieU/Controllers/AdminTvShowsController.cs
@@ -56,7 +56,7 @@ namespace MovieU.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Photo,StreamUrl,Episode")] TvShows tvShows)
+        public async Task<IActionResult> Create([Bind("Id,Name,Photo,StreamUrl,Episode,Tags")] TvShows tvShows)
         {
             if (ModelState.IsValid)
             {
@@ -88,7 +88,7 @@ namespace MovieU.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Photo,StreamUrl,Episode")] TvShows tvShows)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Photo,StreamUrl,Episode, Tags")] TvShows tvShows)
         {
             if (id != tvShows.Id)
             {
