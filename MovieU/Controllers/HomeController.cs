079210e046d76a21270a2b7fd7ef7803b9ec5a77
Abdullah <abdullah.ayoub2000@gmail.com>
2021-06-13 00:02:00 +0200

    General edits on all page home page linked with the db

--- MovieU/Controllers/HomeController.cs
+++ MovieU/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,6 +16,43 @@ namespace MovieU.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _context;
 
+        private void PicRandomElement(IQueryable<Movies> movies, IQueryable<TvShows> tvshows)
+        {
+            // this function picks a random tv show or movie and add it to the view data ....
+            var rand = new Random();
+            int tmp = 0;
+            int rand_conditioning = rand.Next(0, 2);
+            if ( rand_conditioning == 1)
+            {
+                Console.WriteLine("IF !!");
+                var idx = rand.Next(movies.Count());
+                foreach (var element in movies)
+                {
+                    if (tmp == idx)
+                    {
+                        ViewData["TopMovie"] = element;
+                        break;
+                    }
+
+                    tmp++;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Else !!");
+                var idx = rand.Next(tvshows.Count());
+                foreach (var element in tvshows)
+                {
+                    if (tmp == idx)
+                    {
+                        ViewData["TopMovie"] = element;
+                        break;
+                    }
+                    tmp++;
+                }
+            }
+        }
         public HomeController(ApplicationDbContext context)
         {
             _context = context;
@@ -22,8 +60,12 @@ namespace MovieU.Controllers
 
         public IActionResult Index()
         {
+            
             var movies = _context.Movies.OrderByDescending(u => u.Id).Take(4);
+            var tvshows = _context.TvShows.OrderByDescending(t => t.Id).Take(4);
             ViewData["movies"] = movies;
+            ViewData["tvShows"] = tvshows;
+            PicRandomElement(movies, tvshows);
             return View();
         }
 
